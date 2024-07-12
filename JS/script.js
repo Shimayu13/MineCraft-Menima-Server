b92d14cd8058bd4316cf32868552d01adbbe225e
Shimayu13 <yuki.shimazu.0123456789@gmail.com>
2024-07-12 15:08:10 +0900

    edit for smartphone screen( nab Var)

--- JS/script.js
+++ JS/script.js
@@ -5,4 +5,20 @@ window.onload = function() {
         submenu.style.opacity = 0;
         submenu.style.visibility = 'hidden';
     });
+
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const pageLink = document.querySelector('.pageLink');
+    const horizontalLinks = document.querySelectorAll('.horizontal');
+
+    hamburgerMenu.addEventListener('click', function() {
+        pageLink.classList.toggle('active');
+    });
+
+    horizontalLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            if (window.innerWidth <= 600) {
+                this.classList.toggle('active');
+            }
+        });
+    });
 };
